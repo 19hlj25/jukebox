73db314a30baa84c7d04c4ca48c6584702d80b07
19hlj25 <19hlj25@proton.me>
2026-03-16 16:56:36 -0600

    database seeded

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,7 @@
 import db from "#db/client";
+import { createTrack } from "./queries/tracks.js";
+import { createPlaylist } from "./queries/playlists.js";
+import { addTrackToPlaylist } from "./queries/playlists_tracks.js";
 
 await db.connect();
 await seed();
@@ -6,5 +9,29 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  // TODO
+  const tracks = [];
+  const playlists = [];
+
+  for (let i = 0; i < 20; i++) {
+    const track = await createTrack(
+      `Track ${i + 1}`,
+      180000 + i * 10000
+    );
+    tracks.push(track);
+  }
+
+  for (let i = 0; i < 10; i++) {
+    const playlist = await createPlaylist(
+      `Playlist ${i + 1}`,
+      `Description ${i + 1}`
+    );
+    playlists.push(playlist);
+  }
+
+  for (let i = 0; i < 15; i++) {
+    await addTrackToPlaylist(
+      playlists[i % playlists.length].id,
+      tracks[i].id
+    );
+  }
 }
